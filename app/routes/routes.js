945dda251eccc653e5b0506cbda96b22dd39ac9e
Michael Cabelin <ncabelin@gmail.com>
2017-02-08 00:16:54 -0800

    add routes to a different folder

---
+++ app/routes/routes.js
@@ -0,0 +1,27 @@
+var User = require('../models/user');
+
+module.exports = function(router) {
+	router.get('/', function(req, res) {
+		res.sendFile('./public/index.html');
+	});
+
+	router.post('/register', function(req, res) {
+		var user = new User;
+		user.username = req.body.username;
+		user.password = req.body.password;
+		user.email = req.body.email;
+		if (!user.username ||
+			!user.password ||
+			!user.email) {
+			res.status(400).send('Please fill in all fields');
+		} else {
+			user.save(function(err) {
+				if (err) {
+					res.status(400).send('Email, Username already exists');
+				} else {
+					res.json('Registered');
+				}
+			})
+		}
+	});
+}
--- server.js
+++ server.js
@@ -5,36 +5,16 @@ var morgan = require('morgan');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var User = require('./app/models/user');
-var connection = require('./config/database')
+var connection = require('./config/database');
+var router = express.Router();
+var appRoutes = require('./app/routes/routes')(router);
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use('/api', appRoutes);
 
-app.get('/', function(req, res) {
-	res.sendFile('./public/index.html');
-});
-
-app.post('/register', function(req, res) {
-	var user = new User;
-	user.username = req.body.username;
-	user.password = req.body.password;
-	user.email = req.body.email;
-	if (!user.username ||
-		!user.password ||
-		!user.email) {
-		res.status(400).send('Please fill in all fields');
-	} else {
-		user.save(function(err) {
-			if (err) {
-				res.status(400).send('Email, Username already exists');
-			} else {
-				res.json('Registered');
-			}
-		})
-	}
-});
 
 app.listen(port, process.env.IP, function() {
 	console.log('Server started at port ' + port);
